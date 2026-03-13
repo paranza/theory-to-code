3cb26ac9d2f22f9d5a167a650ac961b59633c5ca
paranza <an.perazza@gmail.com>
2026-03-13 09:20:03 +0100

    Enhance comments for clarity on multicollinearity
    Added comments to clarify the purpose of the code and the concept of the 'Dummy Variable Trap'.

--- ols-perfect-multicollinearity.R
+++ ols-perfect-multicollinearity.R
@@ -1,6 +1,8 @@
-#Testing that if a regressor can be described perfectly 
-#with a linear function of another regressor, the ols 
-#gives N/A for that first regressor
+# Testing that if a regressor can be described perfectly 
+# with a linear function of another regressor, the ols 
+# gives N/A for that first regressor
+
+# In this case, we refer to "Dummy Variable Trap"
 
 x = runif(1000,0,4)
 b1 = 1 
@@ -13,4 +15,5 @@ reg1 = rbinom(1000,1,0.3)
 reg2 = 1 - reg1 #dummy var
 
 model = lm(y_noise ~ reg1 + reg2)
+
 summary(model) #reg2 is N/A, linearly dependant from reg1 
